05f00e2bcf9bba41f5010d5d23ffd89c8981f3d9
hkr0416 <hkr0416@gmail.com>
2022-12-06 16:45:51 +0900

    simple route

---
+++ client/script.js
@@ -0,0 +1,9 @@
+function moveToLogin() {
+    location.href = 'index.html'
+}
+function moveToLogin() {
+    location.href = 'login.html'
+}
+function moveToSignup() {
+    location.href = 'signup.html'
+}
